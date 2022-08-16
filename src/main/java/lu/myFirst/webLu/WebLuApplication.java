9be3a4e5d6c827a026c8ecaccbe3bc0987f57799
luayaljumaa <luay.aljumaa@gmail.com>
2022-08-16 14:04:31 +0200

    init

---
+++ src/main/java/lu/myFirst/webLu/WebLuApplication.java
@@ -0,0 +1,13 @@
+package lu.myFirst.webLu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLuApplication.class, args);
+	}
+
+}
---
+++ src/main/java/lu/myFirst/webLu/web/HelloWorldControler.java
@@ -0,0 +1,15 @@
+package lu.myFirst.webLu.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldControler {
+
+    @GetMapping(path = "/")
+    public ModelAndView showHelloWorldPage(){
+        return new ModelAndView("helloworld");
+    }
+
+}
